40de37e7c2764fc41f19ad4f1f30102ee796fcc1
prakriti33 <97353105+prakriti33@users.noreply.github.com>
2025-01-15 11:12:36 +0545

    Update footer.php

--- parent/footer.php
+++ parent/footer.php
@@ -1,8 +1,7 @@
   </div>
   <!-- Main Footer -->
   <footer class="main-footer">
-    <strong>Copyright &copy; 2020 <a href="https://technostudy.co.in">School Managment System</a>.</strong>
-    All rights reserved.
+    
     <div class="float-right d-none d-sm-inline-block">
       <b>Version</b> 1.0
     </div>
